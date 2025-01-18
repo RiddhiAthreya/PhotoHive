f7c011c02f26291386c288c4ab88e52b9362fa3e
Riddhi <riddhi400@gmail.com>
2025-01-18 17:41:20 -0500

    spring init generated files

---
+++ src/test/java/com/NU/riddhi/photoz/clone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NU.riddhi.photoz.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
