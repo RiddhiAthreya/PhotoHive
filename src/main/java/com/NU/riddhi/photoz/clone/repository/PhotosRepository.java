d3789d83e8ea1b6002b5d76ed9a2f5c6f4b95fd7
Riddhi <riddhi400@gmail.com>
2025-01-18 17:40:39 -0500

    Photos repository

---
+++ src/main/java/com/NU/riddhi/photoz/clone/repository/PhotosRepository.java
@@ -0,0 +1,7 @@
+package com.NU.riddhi.photoz.clone.repository;
+
+import com.NU.riddhi.photoz.clone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotosRepository extends CrudRepository<Photo,Integer> {
+}
